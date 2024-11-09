96e5c859cf0cc37c07cb3fd30b403f7e6dad19c8
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-11-09 13:01:35 -0600

    physics

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use flex_load::*;
 fn main() {
     let mut asset_plugin = AssetLoadPlugin::new();
     asset_plugin.add_asset::<Image>("squid", "squid/squiddy-1.png");
-
     asset_plugin.add_asset::<Image>("knife", "knife/knife.png");
+    asset_plugin.add_asset::<Image>("sand", "platforms/sand.png");
 
     let mut app = App::new();
     app.add_plugins((
@@ -39,9 +39,9 @@ fn setup (
             ..Default::default()
         },
         RigidBody::Dynamic,
-        Collider::ball(30.0),
+        Collider::cuboid(25.0, 26.0),
         Velocity::default(),
-        GravityScale(100.0),
+        GravityScale(1.0),
     ));
     commands.spawn(
         SpriteBundle {
@@ -53,5 +53,19 @@ fn setup (
             ..Default::default()
         }
     );
+    commands.spawn((
+        SpriteBundle {
+            texture: loaded.get_typed::<Image>("sand").unwrap(),
+            transform: Transform::from_translation(Vec3::new(0.0, -250.0, 0.0)),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(256.0, 64.0)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid(128.0, 32.0),
+    ));
     commands.spawn(Camera2dBundle::default());
 }
+
