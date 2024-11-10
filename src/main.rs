4feb7cb1a22df226daa9d08b259507b379cc9255
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-11-09 22:50:19 -0600

    funny vals

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,25 @@ pub mod player {
     pub mod ui;
 }
 use player::squid::*;
+
 pub mod objects {
     pub mod knife;
+    pub mod background;
 }
 use objects::knife::*;
+use objects::background::*;
+
+static BACKGROUND_Z: f32 = -100.0;
+static PLATFORM_Z: f32 = -50.0;
+static PLAYER_Z: f32 = 10.0;
+
 fn main() {
     let mut asset_plugin = AssetLoadPlugin::new();
     asset_plugin.add_asset::<Image>("squid", "squid/squiddy_flat.png");
     asset_plugin.add_asset::<Image>("arrow", "squid/squid_arrow_0.png");
     asset_plugin.add_asset::<Image>("knife", "knife/knife.png");
     asset_plugin.add_asset::<Image>("sand", "platforms/sand.png");
-    asset_plugin.add_asset::<Image>("walls", "platforms/walls.png");
+    asset_plugin.add_asset::<Image>("walls", "walls/walls.png");
     asset_plugin.add_asset::<Image>("knife_holder_base", "knife/knife_holder/knife_holder_base.png");
     asset_plugin.add_asset::<Image>("knife_holder_mask_0", "knife/knife_holder/knife_holder_mask_0.png");
     asset_plugin.add_asset::<Image>("knife_holder_mask_1", "knife/knife_holder/knife_holder_mask_1.png");
@@ -31,9 +39,9 @@ fn main() {
         DefaultPlugins.set(ImagePlugin::default_nearest()), 
         asset_plugin, 
         RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(64.0), 
-        RapierDebugRenderPlugin::default(),
+        // RapierDebugRenderPlugin::default(),
         SquidPlugin,
-        KnifePlugin,
+        BackgroundPlugin,
     ));
     app.add_systems(OnEnter(AssetLoadState::Ready), setup);
     app.run();
--- src/objects/background.rs
+++ src/objects/background.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use crate::flex_load::*;
 
 pub struct BackgroundPlugin;
 
@@ -13,20 +14,26 @@ fn spawn_background (
     loaded: Res<LoadedAssets>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let wall_atlas_layout = TextureAtlasLayout::from_grid(UVec2::new(64, 64), Vec2::new(64.0, 64.0), 20);
+    let wall_atlas_layout = texture_atlas_layouts.add(
+        TextureAtlasLayout::from_grid(UVec2::splat(32), 3, 3, None, None)
+    );
 
     for x in -10..10 {
         for y in -10..10 {
             commands.spawn((
                 SpriteBundle {
                     texture: loaded.get_typed::<Image>("walls").unwrap(),
-                    transform: Transform::from_translation(Vec3::new(x as f32 * 64.0, y as f32 * 64.0, 0.0)),
+                    transform: Transform::from_translation(Vec3::new(x as f32 * 64.0, y as f32 * 64.0, -10.0)),
                     sprite: Sprite {
                         custom_size: Some(Vec2::new(64.0, 64.0)),
                         ..Default::default()
                     },
                     ..Default::default()
                 },
+                TextureAtlas {
+                    layout: wall_atlas_layout.clone(),
+                    index: 5,
+                }
             ));
         }
     }
