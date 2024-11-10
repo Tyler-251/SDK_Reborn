be12bb12a1a93709f1c3f5f9a91be7e3f365b899
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-11-10 13:30:06 -0600

    knife

--- src/objects/knife.rs
+++ src/objects/knife.rs
@@ -6,11 +6,11 @@ pub struct KnifePlugin;
 
 impl Plugin for KnifePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AssetLoadState::Ready), spawn_knife);
+        app.add_systems(OnEnter(AssetLoadState::Ready), spawn_knife_holder);
     }
 }
 
-fn spawn_knife (
+fn spawn_knife_holder (
     mut commands: Commands,
     loaded: Res<LoadedAssets>,
 ) {
@@ -27,29 +27,8 @@ fn spawn_knife (
         },
     ));
 
-    commands.spawn( (
-        SpriteBundle {
-            texture: loaded.get_typed::<Image>("knife").unwrap(),
-            transform: Transform::from_translation(Vec3::new(-18.0, 10.0, 0.1)),
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(62.0,32.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    ));
-
-    commands.spawn( (
-        SpriteBundle {
-            texture: loaded.get_typed::<Image>("knife").unwrap(),
-            transform: Transform::from_translation(Vec3::new(-18.0, -10.0, 0.1)),
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(62.0,32.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    ));
+    spawn_knife(&mut commands, &loaded, Vec3::new(-18.0, 10.0, 0.1));
+    spawn_knife(&mut commands, &loaded, Vec3::new(-18.0, -10.0, 0.1));
 
     commands.spawn( (
         SpriteBundle {
@@ -63,41 +42,9 @@ fn spawn_knife (
         },
     )); 
 
-    commands.spawn( (
-        SpriteBundle {
-            texture: loaded.get_typed::<Image>("knife").unwrap(),
-            transform: Transform::from_translation(Vec3::new(-10.0, 13.0, 0.3)),
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(62.0,32.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    ));
-
-    commands.spawn( (
-        SpriteBundle {
-            texture: loaded.get_typed::<Image>("knife").unwrap(),
-            transform: Transform::from_translation(Vec3::new(-10.0, -1.0, 0.3)),
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(62.0,32.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    ));
-
-    commands.spawn( (
-        SpriteBundle {
-            texture: loaded.get_typed::<Image>("knife").unwrap(),
-            transform: Transform::from_translation(Vec3::new(-10.0, -13.0, 0.3)),
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(62.0,32.0)),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    ));
+    spawn_knife(&mut commands, &loaded, Vec3::new(-10.0, 13.0, 0.3));
+    spawn_knife(&mut commands, &loaded, Vec3::new(-10.0, -1.0, 0.3));
+    spawn_knife(&mut commands, &loaded, Vec3::new(-10.0, -13.0, 0.3));
 
     commands.spawn( (
         SpriteBundle {
@@ -110,4 +57,33 @@ fn spawn_knife (
             ..Default::default()
         },
     )); 
+}
+
+#[derive(Component)]
+pub struct Knife;
+
+#[derive(Component)]
+pub struct KnifeGroup {
+    pub knives: Vec<Entity>,
+}
+
+fn spawn_knife (
+    mut commands: &mut Commands,
+    loaded: &Res<LoadedAssets>,
+    starting_pos: Vec3
+) {
+    commands.spawn( (
+        Knife,
+        SpriteBundle {
+            texture: loaded.get_typed::<Image>("knife").unwrap(),
+            transform: Transform::from_translation(starting_pos),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(62.0,32.0)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Collider::cuboid(31., 10.),
+        Sensor,
+    ));
 }
