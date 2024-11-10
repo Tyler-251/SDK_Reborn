cbed26c44c737d1d2ae37300b6700fcefa76063c
Tyler <tyler.wayne2002@hotmail.com>
2024-11-10 15:23:36 -0600

    lagging!!!!

--- src/objects/knife.rs
+++ src/objects/knife.rs
@@ -15,21 +15,27 @@ pub fn init_test (
     mut commands: Commands,
     loaded: Res<LoadedAssets>,
 ) {
-    spawn_knife_holder(commands, loaded, Vec2::ZERO);
+    spawn_knife_holder(commands, loaded, Vec2::ZERO, KnifeHolder::new_debug(2.0));
 }
 
 // #region Knife Holder
 #[derive(Component)]
+pub struct KnifeDebugText;
+
+#[derive(Component, Clone)]
 pub struct KnifeHolder{
     pub timer: Timer,
-    pub index: u8,
+    pub index: i8,
+    debug: bool
 }
+
 
 impl Default for KnifeHolder {
     fn default() -> Self {
         Self {
             timer: Timer::from_seconds(2.0, TimerMode::Repeating),
-            index: 0,
+            index: -1,
+            debug: false,
         }
     }
 }
@@ -39,6 +45,14 @@ impl KnifeHolder {
         Self {
             timer: Timer::from_seconds(seconds, TimerMode::Repeating),
             index: 0,
+            debug: false,
+        }
+    }
+    pub fn new_debug (seconds: f32) -> Self {
+        Self {
+            timer: Timer::from_seconds(seconds, TimerMode::Repeating),
+            index: 0,
+            debug: true,
         }
     }
 }
@@ -48,7 +62,8 @@ fn tick_knife_holders (
     loaded: Res<LoadedAssets>,
     time: Res<Time>,
     mut query: Query<(&mut KnifeHolder, &Children, Entity)>,
-    mut velocity_query: Query<(&mut Velocity, &Knife, &Transform)>,
+    mut knife_query: Query<(&mut Velocity, &mut Knife, &Transform, Entity)>,
+    mut text_query: Query<&mut Text, With<KnifeDebugText>>,
 ) {
     for (mut knife_holder, children, entity) in query.iter_mut() {
         knife_holder.timer.tick(time.delta());
@@ -56,16 +71,26 @@ fn tick_knife_holders (
             knife_holder.index = (knife_holder.index + 1) % 5;
             
             for child in children.iter() {
-                if let Ok((mut knife_velocity, knife_struct, knife_transform)) = velocity_query.get_mut(*child) {
-                    if knife_struct.0 == knife_holder.index {
+                if let Ok((mut knife_velocity, knife_struct, knife_transform, _)) = knife_query.get_mut(*child) {
+                    if knife_struct.index == knife_holder.index {
                         knife_velocity.linvel = Vec2::new(-300.0, 0.0);
                         let old_position = knife_transform.translation;
                         commands.entity(entity).with_children(|parent| {
-                            spawn_knife(parent, loaded.get_typed("knife").unwrap(), old_position, knife_struct.0);
+                            spawn_knife(parent, loaded.get_typed("knife").unwrap(), old_position, knife_struct.index);
                         });
                     }
                 }
+                if let Ok(mut text) = text_query.get_mut(*child) {
+                    text.sections[0].value = format!("{}", knife_holder.index);
+                }
             }
+        }
+    }
+    for (_, mut knife, _, knife_entity) in knife_query.iter_mut() {
+        if knife.ttl <= 0.0 {
+            commands.entity(knife_entity).despawn();
+        } else {
+            knife.ttl -= time.delta_seconds();
         }
     }
 }
@@ -75,10 +100,11 @@ pub fn spawn_knife_holder (
     mut commands: Commands,
     loaded: Res<LoadedAssets>,
     position: Vec2,
+    knife_holder: KnifeHolder,
 ) {
     //knife base
     commands.spawn( (
-        KnifeHolder::default(),
+        knife_holder.clone(),
         SpriteBundle {
             texture: loaded.get_typed::<Image>("knife_holder_base").unwrap(),
             transform: Transform::from_translation(position.extend(0.)),
@@ -122,20 +148,36 @@ pub fn spawn_knife_holder (
         spawn_knife(parent, loaded.get_typed("knife").unwrap(), Vec3::new(-10.0, -1.0, 0.3), 1);
         spawn_knife(parent, loaded.get_typed("knife").unwrap(), Vec3::new(-10.0, -13.0, 0.3), 0);
 
+        if knife_holder.debug {
+            parent.spawn((
+                KnifeDebugText,
+                Text2dBundle {
+                    text: Text::from_section("test", TextStyle {
+                        color: Color::BLACK,
+                        ..default()
+                    }),
+                    transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.5)),
+                    ..Default::default()
+                }
+            ));
+        }
     });
 }
 
 #[derive(Component)]
-pub struct Knife(u8);
+pub struct Knife {
+    pub index: i8,
+    pub ttl: f32
+}
 
 fn spawn_knife (
-    mut child_builder: &mut ChildBuilder,
+    child_builder: &mut ChildBuilder,
     sprite: Handle<Image>,
     starting_pos: Vec3,
-    index: u8,
+    index: i8,
 ) {
     child_builder.spawn( (
-        Knife(index),
+        Knife{index, ttl: 20.0},
         SpriteBundle {
             texture: sprite,
             transform: Transform::from_translation(starting_pos),
