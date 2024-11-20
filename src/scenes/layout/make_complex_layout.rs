e8d3a1c1cee0df36d82db928a4d832fb533dffe5
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-11-20 00:42:54 -0600

    complex room impled

--- src/scenes/layout/make_complex_layout.rs
+++ src/scenes/layout/make_complex_layout.rs
@@ -10,48 +10,83 @@ pub struct ComplexLayoutPlugin;
 impl Plugin for ComplexLayoutPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(ComplexLayout::new());
-        app.add_systems(OnEnter(AssetLoadState::Ready), (generate, render_ghost_rooms).chain());
+        app.add_systems(OnEnter(AssetLoadState::Ready), generate);
+        app.add_systems(Update, (manually_gen, render_ghost_rooms).chain());
     }
 }
 
+pub fn manually_gen (
+    mut commands: Commands,
+    mut layout: ResMut<ComplexLayout>,
+    inputs: Res<ButtonInput<KeyCode>>,
+    mut display_query: Query<(&FauxDisplay, Entity)>,
+) {
+    if inputs.just_pressed(KeyCode::KeyF) {
+        layout.rooms = vec![];
+        for (_display, entity) in display_query.iter_mut() {
+            commands.entity(entity).despawn_recursive();
+        }
+        generate(layout);
+    }
+}
+
+
+
+#[derive(Component)]
+pub struct FauxDisplay;
+
 pub fn render_ghost_rooms (
     mut commands: Commands,
-    layout: Res<ComplexLayout>,
+    layout: ResMut<ComplexLayout>,
+    mut display_query: Query<(&FauxDisplay, Entity)>,
 ) {
+    if display_query.iter().count() > 0 {
+        return;
+    }
     for room in layout.rooms.iter() {
+        let mut rng = rand::thread_rng();
+        let room_color = Color::srgb(
+            rng.gen_range(0.0..1.0), 
+            rng.gen_range(0.0..1.0),
+            rng.gen_range(0.0..1.0)
+        );
         for chunk in room.chunks.iter() {
-            commands.spawn(
+            commands.spawn((
+                FauxDisplay,
                 SpriteBundle {
                     sprite: Sprite {
+                        color: room_color,
                         custom_size: Some(Vec2::new(90.0, 90.0)),
                         ..default()
                     },
                     transform: Transform::from_translation(Vec3::new(chunk.x as f32 * 100.0, chunk.y as f32 * 100.0, 0.0)),
                     ..default()
                 }
-            );
+            ));
         }
-        // for door in room.doors.iter() {
-        //     let from = door.from.as_vec2();
-        //     let to = door.to.as_vec2();
-        //     let midpoint = from.lerp(to, 0.5);
-        //     let direction = to - from;
-        //     let angle = direction.y.atan2(direction.x);
-        //     commands.spawn(
-        //         SpriteBundle {
-        //             sprite: Sprite {
-        //                 custom_size: Some(Vec2::new(90.0, 90.0)),
-        //                 ..default()
-        //             },
-        //             transform: Transform {
-        //                 translation: Vec3::new(midpoint.x as f32 * 100.0, midpoint.y as f32 * 100.0, 0.0),
-        //                 rotation: Quat::from_rotation_z(angle),
-        //                 ..default()
-        //             },
-        //             ..default()
-        //         }
-        //     );
-        // }
+        for door in room.doors.iter() {
+            let from = door.from.as_vec2();
+            let to = door.to.as_vec2();
+            let midpoint = from.lerp(to, 0.5);
+            let direction = to - from;
+            let angle = direction.y.atan2(direction.x);
+            commands.spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        custom_size: Some(Vec2::new(10.0, 10.0)),
+                        color: Color::srgb(0.0, 1.0, 0.0),
+                        ..default()
+                    },
+                    transform: Transform {
+                        translation: Vec3::new(midpoint.x as f32 * 100.0, midpoint.y as f32 * 100.0, 0.0),
+                        rotation: Quat::from_rotation_z(angle),
+                        ..default()
+                    },
+                    ..default()
+                },
+                FauxDisplay
+            ));
+        }
     }
 }
 
@@ -93,13 +128,22 @@ pub fn generate (
 
         if valid_placements.len() == 0 { // if room is blocked in, pick a new room and start over
             active_room = layout.rooms[rng.gen_range(0..layout.rooms.len())].clone();
+            println!("this should not happen often");
             continue;
         }
 
         let new_chunk = *valid_placements[rng.gen_range(0..valid_placements.len())];
-        let new_room = generate_room(new_chunk, 3, &layout);
+        let new_room = generate_room(new_chunk, rng.gen_range(1..=3), &layout);
         layout.rooms.push(new_room.clone());
         layout.insert_door(active_room.clone(), new_room);
+    }
+    // add more doors
+    for room in layout.rooms.clone().iter() {
+        for other_room in layout.rooms.clone().iter() {
+            if room.is_adjacent(other_room) && rng.gen_bool(0.3) {
+                layout.insert_door(room.clone(), other_room.clone());
+            }
+        }
     }
 }
 
@@ -147,6 +191,16 @@ impl ComplexRoom {
 
         return chunk_list;
     }
+    fn is_adjacent(&self, other: &ComplexRoom) -> bool {
+        for chunk in self.chunks.iter() {
+            for other_chunk in other.chunks.iter() {
+                if chunk.as_vec2().distance(other_chunk.as_vec2()) == 1.0 {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct ComplexRoomDoor {
@@ -154,7 +208,7 @@ pub struct ComplexRoomDoor {
     pub to: IVec2,
 }
 
-#[derive(Resource)]
+#[derive(Resource, Clone)]
 pub struct ComplexLayout {
     pub rooms: Vec<ComplexRoom>,
 }
@@ -206,6 +260,7 @@ impl ComplexLayout {
         let choice = possible_doors[rng.gen_range(0..possible_doors.len())];
         self.insert_door_by_chunks(choice.0, choice.1);
     }
+    
 }
 
 fn random_unit_vector() -> IVec2 {
